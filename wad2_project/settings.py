6738e7990d2ab941beb337f78ef45a5133fecb2d
Jakub Suriak <2482569S@student.gla.ac.uk>
2021-03-11 11:46:44 +0000

    Edited settings.py so that I can serve static files on pythonanywhere

--- wad2_project/settings.py
+++ wad2_project/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_DIR = STATIC_ROOT
 
 STATIC_URL = '/static/'
 
