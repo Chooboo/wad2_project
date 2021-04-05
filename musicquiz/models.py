2742c16faade76a12e688aaa4ac99e784b3f2e75
Jakub Suriak <2482569S@student.gla.ac.uk>
2021-04-05 20:13:54 +0100

    Improved styles of About and Profile pages

--- musicquiz/models.py
+++ musicquiz/models.py
@@ -7,7 +7,6 @@ from django.utils import timezone
 class MusicCategory(models.Model):
     title = models.CharField(max_length=50, unique=True)
     description = models.TextField()
-    user_count = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
     image_name = models.CharField(max_length=200)
 
@@ -25,19 +24,18 @@ class MusicCategory(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to='profile_images', default="profile_images/default_profile_pic.png")
+    category = models.ForeignKey(MusicCategory, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.user.username
 
 
 class Comment(models.Model):
-    category = models.ForeignKey(MusicCategory,
-                                 on_delete=models.CASCADE,
-                                 blank=False)
+    category = models.ForeignKey(MusicCategory, blank=False, on_delete=models.CASCADE)
     author = models.ForeignKey(UserProfile, related_name="author", blank=False, on_delete=models.CASCADE)
     body = models.CharField(max_length=300, blank=False)
     date_added = models.DateTimeField(default=timezone.now)
-    likes = models.ManyToManyField(UserProfile, related_name="liked_by", blank=True)
+    likes = models.ManyToManyField(UserProfile, related_name="liked_by", null=True)
 
     def __str__(self):
         return self.body[:20]
