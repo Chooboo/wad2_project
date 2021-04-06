f8d171f092ecf5729945fcb1eeecfaf66b92e6fe
Jakub Suriak <2482569S@student.gla.ac.uk>
2021-04-06 11:28:55 +0100

    Input control on the quiz, better responsive layout

--- static/assets/js/quiz-ajax.js
+++ static/assets/js/quiz-ajax.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
         let questionId = parseInt($(this).attr('data-questionid'));
         let points = parseInt($("#point-counter").text());
         let newPoints = parseInt($("input[type='radio']:checked").val());
-        if (questionId !== 3){
-            $.get('/quiz/question/' + (questionId + 1) + '/',
-                {'points': points + newPoints},
-                function (data) {
-                    $('#quiz-content').html(data);
-                })
-        } else {
-            document.location.href = '/quiz/quizresults/' + (points + newPoints) + '/';
+        if (!isNaN(newPoints)){
+            if (questionId !== 3) {
+                $.get('/quiz/question/' + (questionId + 1) + '/',
+                    {'points': points + newPoints},
+                    function (data) {
+                        $('#quiz-content').html(data);
+                    })
+            } else {
+                document.location.href = '/quiz/quizresults/' + (points + newPoints) + '/';
+            }
         }
     });
     $("body").on("click", "#start-quiz", function(){
